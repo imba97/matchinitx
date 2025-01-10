489f971f6695c8a4b2eeb9daa4a5cb072dbe9b42
imba97 <mail@imba97.cn>
2025-01-11 00:59:48 +0800

    test: use resultTypeFn test

--- tests/type-matcher.test.ts
+++ tests/type-matcher.test.ts
@@ -1,20 +1,44 @@
 import { describe, expect, it } from 'vitest'
 import { type MatcherRules, useInitxMatcher } from '../src/index'
 
-function resultFn(rule: MatcherRules, ...others: string[]) {
+enum CustomType {
+  FOO = 'foo',
+  BAR = 'bar'
+}
+
+function resultFn(rule: any, ...others: string[]) {
   return { rule, others }
 }
 
-describe('initxTypeMatcher', () => {
-  const matcher = useInitxMatcher(resultFn)
+function resultTypeFn(rule: any, type: string, ...others: string[]) {
+  return { rule, type, others }
+}
 
+describe('initxTypeMatcher', () => {
   it('should match with TypeMatchers', () => {
-    const rules = {
+    const matcher = useInitxMatcher(resultFn)
+
+    const rules: MatcherRules = {
       type1: { matching: 'testKey1' },
       type2: { matching: 'testKey2' }
     }
 
     const result = matcher.match(rules, 'testKey2', 'extra2')
     expect(result).toEqual([{ rule: {}, others: ['type2', 'extra2'] }])
+  })
+
+  it('use resultTypeFn', () => {
+    const matcher = useInitxMatcher(resultTypeFn)
+
+    const rules: MatcherRules = {
+      [CustomType.FOO]: { matching: 'testKey1' },
+      [CustomType.BAR]: { matching: 'testKey2' }
+    }
+
+    const result1 = matcher.match(rules, 'testKey1', 'extra')
+    expect(result1).toEqual([{ rule: {}, type: CustomType.FOO, others: ['extra'] }])
+
+    const result2 = matcher.match(rules, 'testKey2', 'extra')
+    expect(result2).toEqual([{ rule: {}, type: CustomType.BAR, others: ['extra'] }])
   })
 })
